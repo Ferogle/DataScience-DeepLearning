10c37eb3b3d69bcd8bf1b410671a1fdcb114294e
Ferogle <55873896+Ferogle@users.noreply.github.com>
2020-02-03 22:53:00 +0530

    Update PoliceActivity.py

--- PoliceActivity.py
+++ PoliceActivity.py
@@ -1,3 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  2 10:22:06 2019
+
+@author: srikar
+"""
+#analysing data
+# Import the pandas library as pd
+import pandas as pd
+# Read 'police.csv' into a DataFrame named ri
+ri = pd.read_csv('police.csv')
+# Examine the head of the DataFrame
+print(ri.head())
+# Count the number of missing values in each column
+print(ri.isnull().sum())
+# Examine the shape of the DataFrame
+#------------------------------------------------------------
+print(ri.shape)
+# Drop the 'county_name' and 'state' columns
+ri.drop(['county_name','state'], axis='columns', inplace=True)
+# Examine the shape of the DataFrame (again)
+print(ri.shape)
+#-----------------------------------------------------------
+# Count the number of missing values in each column
+print(ri.isnull().sum())
+# Drop all rows that are missing 'driver_gender'
+ri.dropna(subset=['driver_gender'], inplace=True)
+# Count the number of missing values in each column (again)
+print(ri.isnull().sum())
+# Examine the shape of the DataFrame
+print(ri.shape)
+# Examine the head of the 'is_arrested' column
+#-------------------------------------------------------------------
+print(ri.is_arrested.dtype)
+# Change the data type of 'is_arrested' to 'bool'
+ri['is_arrested'] = ri.is_arrested.astype('bool')
+# Check the data type of 'is_arrested' 
+print(ri.is_arrested.dtype)
+#-------------------------------------------------------------------
+# Concatenate 'stop_date' and 'stop_time' (separated by a space)
+combined = ri.stop_date.str.cat(ri.stop_time,sep=' ')
+# Convert 'combined' to datetime format
+ri['stop_datetime'] = pd.to_datetime(combined)
+# Examine the data types of the DataFrame
+print(ri.dtypes)
+#--------------------------------------------------------------------
+# Set 'stop_datetime' as the index
+ri.set_index('stop_datetime', inplace=True)
+# Examine the index
+print(ri.index)
+# Examine the columns
+print(ri.columns)
+#-----------------------------------------------------------------
+#do genders commit different violations
+#-----------------------------------------------------------------
+# Count the unique values in 'violation','driver_race','search_outcome'
+print(ri.violation.value_counts())
+# Express the counts as proportions
+print(ri.violation.value_counts(normalize=True))
+#------------------------------------------------------------------------------
+# Create a DataFrame of female drivers
+female = ri[ri.driver_gender=='F']
+# Create a DataFrame of male drivers
+male = ri[ri.driver_gender=='M']
+# Compute the violations by female drivers (as proportions)
+print(female.violation.value_counts(normalize=True))
+# Compute the violations by male drivers (as proportions)
+print(male.violation.value_counts(normalize=True))
+#------------------------------------------------------------------------------
+'''When a driver is pulled over for speeding, many people believe that gender has an 
+impact on whether the driver will receive a ticket or a warning. Can you find evidence
+ of this in the dataset?'''
+ #-----------------------------------------------------------------------------
+ # Create a DataFrame of female drivers stopped for speeding
+female_and_speeding = ri[(ri.driver_gender=='F') & (ri.violation=='Speeding')]
+# Create a DataFrame of male drivers stopped for speeding
+male_and_speeding = ri[(ri.driver_gender=='M') & (ri.violation=='Speeding')]
+# Compute the stop outcomes for female drivers (as proportions)
+print(female_and_speeding.stop_outcome.value_counts(normalize=True))
+# Compute the stop outcomes for male drivers (as proportions)
+print(male_and_speeding.stop_outcome.value_counts(normalize=True))
+#------------------------------------------------------------------------------
+# numbers were similar for both male and female and about 95% of stops due to speeding resulted
+# in ticket
+# -----------------------------------------------------------------------------
+# does gender affect whose vehicle is searched?
+#------------------------------------------------------------------------------
+# Check the data type of 'search_conducted'
+print(ri.search_conducted.dtype)
+# Calculate the search rate by counting the values
+print(ri.search_conducted.value_counts(normalize=True))
+# Calculate the search rate by taking the mean
+print(ri.search_conducted.mean())
+#------------------------------------------------------------------------------
+# Calculate the search rate for female drivers
+print(ri[ri.driver_gender=='F'].search_conducted.mean())
+# Calculate the search rate for male drivers
+print(ri[ri.driver_gender=='M'].search_conducted.mean())
+# Calculate the search rate for both groups simultaneously
+print(ri.groupby('driver_gender').search_conducted.mean())
+#Wow! Male drivers are searched more than twice as often as female drivers. Why might this be
+#------------------------------------------------------------------------------
+# Calculate the search rate for each combination of gender and violation
+print(ri.groupby(['driver_gender', 'violation']).search_conducted.mean())
+# Reverse the ordering to group by violation before gender
+print(ri.groupby(['violation','driver_gender']).search_conducted.mean())
+#------------------------------------------------------------------------------
+'''Great work! For all types of violations, 
+the search rate is higher for males than for females, disproving our hypothesis.'''
 # you'll first check to see how many times "Protective Frisk" was the only search type.
 # Count the 'search_type' values
 print(ri['search_type'].value_counts())

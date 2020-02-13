428384d55daf0f7ec255a6504997aa2e1cef628e
Ferogle <55873896+Ferogle@users.noreply.github.com>
2020-02-14 01:55:16 +0530

    Create CaseStudy-Darwin's_Finches.py

---
+++ CaseStudy-Darwin's_Finches.py
@@ -0,0 +1,123 @@
+# observing the evolution of beak length and beak depth of finches' 
+
+# Create bee swarm plot
+_ = sns.swarmplot(x='year',y='beak_depth',data=df)
+
+# Label the axes
+_ = plt.xlabel('year')
+_ = plt.ylabel('beak depth (mm)')
+
+# Show the plot
+plt.show()
+
+# Compute ECDFs
+x_1975, y_1975 = ecdf(bd_1975)
+x_2012, y_2012 = ecdf(bd_2012)
+
+# Plot the ECDFs
+_ = plt.plot(x_1975, y_1975, marker='.', linestyle='none')
+_ = plt.plot(x_2012, y_2012, marker='.', linestyle='none')
+
+# Set margins
+plt.margins(0.02)
+
+# Add axis labels and legend
+_ = plt.xlabel('beak depth (mm)')
+_ = plt.ylabel('ECDF')
+_ = plt.legend(('1975', '2012'), loc='lower right')
+
+# Show the plot
+plt.show()
+
+'''The differences are much clearer in the ECDF. The 
+mean is larger in the 2012 data, and the variance does appear larger as well.'''
+# Compute the difference of the sample means: mean_diff
+mean_diff = np.mean(bd_2012)-np.mean(bd_1975)
+
+# Get bootstrap replicates of means
+bs_replicates_1975 = draw_bs_reps(bd_1975,np.mean,10000)
+bs_replicates_2012 = draw_bs_reps(bd_2012,np.mean,10000)
+
+# Compute samples of difference of means: bs_diff_replicates
+bs_diff_replicates = bs_replicates_2012 -  bs_replicates_1975
+
+# Compute 95% confidence interval: conf_int
+conf_int = np.percentile(bs_diff_replicates,[2.5,97.5])
+
+# Print the results
+print('difference of means =', mean_diff, 'mm')
+print('95% confidence interval =', conf_int, 'mm')
+
+# Compute mean of combined data set: combined_mean
+combined_mean = np.mean(np.concatenate((bd_1975, bd_2012)))
+
+# Shift the samples
+bd_1975_shifted = bd_1975-np.mean(bd_1975)+combined_mean
+bd_2012_shifted = bd_2012-np.mean(bd_2012)+combined_mean
+
+# Get bootstrap replicates of shifted data sets
+bs_replicates_1975 = draw_bs_reps(bd_1975_shifted,np.mean,size=10000)
+bs_replicates_2012 = draw_bs_reps(bd_2012_shifted,np.mean,size=10000)
+
+# Compute replicates of difference of means: bs_diff_replicates
+bs_diff_replicates = bs_replicates_2012-bs_replicates_1975
+
+# Compute the p-value
+p = np.sum(bs_diff_replicates >= mean_diff) / len(bs_diff_replicates)
+
+# Print p-value
+print('p =', p)
+
+'''We get a p-value of 0.0034, which suggests that there is a statistically significant difference. But remember: 
+it is very important to know how different they are! In the previous exercise,
+you got a difference of 0.2 mm between the means. You should combine this with the statistical significance. 
+Changing by 0.2 mm in 37 years is substantial by evolutionary standards. 
+If it kept changing at that rate, the beak depth would double in only 400 years.'''
+
+# Make scatter plot of 1975 data
+_ = plt.scatter(bl_1975, bd_1975, marker='.',
+             linestyle='None', alpha='0.2', color='blue')
+
+# Make scatter plot of 2012 data
+_ = plt.scatter(bl_2012, bd_2012, marker='.',
+             linestyle='None', alpha='0.2', color='red')
+
+# Label axes and make legend
+_ = plt.xlabel('beak length (mm)')
+_ = plt.ylabel('beak depth (mm)')
+_ = plt.legend(('1975', '2012'), loc='upper left')
+
+# Show the plot
+plt.show()
+
+'''Great work! In looking at the plot, we see that beaks got deeper (the red points are higher up in the y-direction), 
+but not really longer. 
+If anything, they got a bit shorter, since the red dots are to the left of the blue dots. 
+So, it does not look like the beaks kept the same shape; they became shorter and deeper.'''
+# Compute the linear regressions
+slope_1975, intercept_1975 = np.polyfit(bl_1975, bd_1975, deg=1)
+slope_2012, intercept_2012 = np.polyfit(bl_2012, bd_2012, deg=1)
+
+# Perform pairs bootstrap for the linear regressions
+bs_slope_reps_1975, bs_intercept_reps_1975 = \
+        draw_bs_pairs_linreg(bl_1975, bd_1975, size=1000)
+bs_slope_reps_2012, bs_intercept_reps_2012 = \
+        draw_bs_pairs_linreg(bl_2012, bd_2012, size=1000)
+
+# Compute confidence intervals of slopes
+slope_conf_int_1975 = np.percentile(bs_slope_reps_1975,[2.5,97.5])
+slope_conf_int_2012 = np.percentile(bs_slope_reps_2012,[2.5,97.5])
+intercept_conf_int_1975 = np.percentile(bs_intercept_reps_1975,[2.5,97.5])
+
+intercept_conf_int_2012 = np.percentile(bs_intercept_reps_2012,[2.5,97.5])
+
+
+# Print the results
+print('1975: slope =', slope_1975,
+      'conf int =', slope_conf_int_1975)
+print('1975: intercept =', intercept_1975,
+      'conf int =', intercept_conf_int_1975)
+print('2012: slope =', slope_2012,
+      'conf int =', slope_conf_int_2012)
+print('2012: intercept =', intercept_2012,
+      'conf int =', intercept_conf_int_2012)
